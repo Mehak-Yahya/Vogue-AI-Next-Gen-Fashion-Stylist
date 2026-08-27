efc422e8aa9716006f551ca44b4ea0c40ab9c513
Mehak Yahya <kandhirmehak@gmail.com>
2026-08-27 22:14:57 +0500

    Landing Page

---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,12 @@
+import Hero from "../components/Hero/Hero";
+import SkinAnalysis from "../components/Hero/SkinAnalysis";
+
+export default function Landing() {
+  return (
+    <main>
+
+      <Hero />
+      <SkinAnalysis />
+    </main>
+  );
+}
