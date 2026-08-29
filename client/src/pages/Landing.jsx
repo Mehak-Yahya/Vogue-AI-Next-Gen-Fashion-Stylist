dd3677cbf307e64342c306f49868f784ca4fc0c7
Mehak Yahya <kandhirmehak@gmail.com>
2026-08-30 02:37:21 +0500

    Landing page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,12 +1,14 @@
 import Hero from "../components/Hero/Hero";
 import SkinAnalysis from "../components/Hero/SkinAnalysis";
-
+import VirtualWardrobe from "../components/Hero/VirtualWardrobe";
+import AIChatHero from "../components/Hero/AIChatHero";
 export default function Landing() {
   return (
-    <main>
-
+    <main className="page-shell">
       <Hero />
       <SkinAnalysis />
+      <VirtualWardrobe />
+      <AIChatHero />
     </main>
   );
 }
