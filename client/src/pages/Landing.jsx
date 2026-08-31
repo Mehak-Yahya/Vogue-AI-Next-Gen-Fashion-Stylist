f881c824f464ba9e2d94e38baadb2480c9ee01d4
Mehak Yahya <kandhirmehak@gmail.com>
2026-08-31 20:58:23 +0500

    update

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -2,6 +2,8 @@ import Hero from "../components/Hero/Hero";
 import SkinAnalysis from "../components/Hero/SkinAnalysis";
 import VirtualWardrobe from "../components/Hero/VirtualWardrobe";
 import AIChatHero from "../components/Hero/AIChatHero";
+import AboutVogue from "../components/Hero/AboutVogue";
+import Footer from "../components/Hero/Footer";
 export default function Landing() {
   return (
     <main className="page-shell">
@@ -9,6 +11,8 @@ export default function Landing() {
       <SkinAnalysis />
       <VirtualWardrobe />
       <AIChatHero />
+      <AboutVogue />
+      <Footer />
     </main>
   );
 }
